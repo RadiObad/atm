d254e3258afc29305e0515f73fdee78c33015533
Radi_Mohemmad_Obad <lovelovelove67@gmail.com>
2018-07-15 00:36:58 +0300

    Test anthor solution

--- atm.py
+++ atm.py
@@ -2,35 +2,20 @@
 
 amount = 800
 
-request = 567
-
+#request = 300
+request = 721
+#request = 555	
+lst = [100,50,10,5,2,1]
 if request > amount:
 	print "Can't give you all this menoy !!"
 elif request < 0:
 	print "More than zero plz!"
-else:		 
-	while request > 0:
+else:
+	for p in lst:
+		while request >= p:
+			print "give " + str(p)
+			request -= p
 
-		if request > 100:
-			print "give 100"
-			request -= 100
-		
-		elif request > 50:
-			print"give 50"
-			request -= 50
-		
-		elif request > 10:
-			print"give 10"
-			request -= 10
-		
-		elif request > 5:
-			print"give 5"
-			request -= 5
-		
-		else:
-			print "give " + str(request)
-			request -= request	
-	#request = 0		
-print request	
-			
+
+
 	
