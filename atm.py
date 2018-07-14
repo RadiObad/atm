34df4ce2290f1c6d2331e605823a50a3a787a562
Radi_Mohemmad_Obad <lovelovelove67@gmail.com>
2018-07-14 23:42:02 +0300

    Upadte code atm

--- atm.py
+++ atm.py
@@ -1,23 +1,35 @@
-the_amount = 500
-request = 277
-resulat = the_amount - request
-while request > 0:
-	if request > 100:
-		print "give" + str(100)
-		request = request - 100
-	elif request > 50:
-		print"give" + str(50)
-		request = request -50
-	elif request > 10:
-		print"give" + str(10)
-		request = request -10
-	elif request > 5:
-		print"give" + str(5)
-		request = request -5
+# allowed papers: 100, 50, 10, 5, and cents
+
+amount = 800
+
+request = 567
+
+if request > amount:
+	print "Can't give you all this menoy !!"
+elif request < 0:
+	print "More than zero plz!"
+else:		 
+	while request > 0:
+
+		if request > 100:
+			print "give 100"
+			request -= 100
 		
-	else:
-		print "give" + str(request)
-		request = request -request	
+		elif request > 50:
+			print"give 50"
+			request -= 50
+		
+		elif request > 10:
+			print"give 10"
+			request -= 10
+		
+		elif request > 5:
+			print"give 5"
+			request -= 5
+		
+		else:
+			print "give " + str(request)
+			request -= request	
 	#request = 0		
 print request	
 			
