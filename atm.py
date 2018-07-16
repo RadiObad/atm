5e3823acf84549766906292c2f2e44739d18ae9d
Radi_Mohemmad_Obad <lovelovelove67@gmail.com>
2018-07-16 19:27:45 +0300

    Adedd classes with work

--- atm.py
+++ atm.py
@@ -1,45 +1,48 @@
 # allowed papers: 100, 50, 10, 5, and cents
 
-amount = 200
 
-#request = 567
+class ATM():
+	amount = 200
+	def __init__(self, balance, bank_name):
+		self.balance = balance	
+		self.bank_name	= bank_name	
 
-def withdraw(amount,request):	
-	Balance_restante = amount - request
-	if request > amount:
-		print "Can't give you all this menoy !!"
-	elif request < 0:
-		print "More than zero plz!"
-	else:		 
-		while request > 0:
+	def withdraw(self, request):	
+		self.balance = self.balance - request
+		if request > self.balance:
+			print "Can't give you all this menoy !!"
+		elif request < 0:
+			print "More than zero plz!"
+		else:		 
+			while request > 0:
 
-			if request >= 100:
-				print "give 100"
-				request -= 100
-			
-			elif request >= 50:
-				print"give 50"
-				request -= 50
-			
-			elif request >= 10:
-				print"give 10"
-				request -= 10
-			
-			elif request >= 5:
-				print"give 5"
-				request -= 5
-			
-			else:
-				print "give " + str(request)
-				request -= request	
-		#request = 0		
-	return Balance_restante	
-			
-amount =  withdraw(amount,3)
-print amount
+				if request >= 100:
+					print "give 100"
+					request -= 100
+				
+				elif request >= 50:
+					print"give 50"
+					request -= 50
+				
+				elif request >= 10:
+					print"give 10"
+					request -= 10
+				
+				elif request >= 5:
+					print"give 5"
+					request -= 5
+				
+				else:
+					print "give " + str(request)
+					request -= request	
+			#request = 0		
+		return self.balance	
 
-#amount = withdraw(amount,30)
-#print amount
 
-#amount = withdraw(amount,5)
-#print amount
+atm1 = ATM(0,"Smart Bank")			
+print atm1.withdraw(223)			
+print atm1.withdraw(900)	
+
+
+#atm2 = ATM(500, "Any ?Bank")
+#atm2.withdraw(233)
