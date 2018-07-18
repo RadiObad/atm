3601dd97dc295d6f98115dfe48167fb0bead0d13
Radi_Mohemmad_Obad <lovelovelove67@gmail.com>
2018-07-18 17:15:58 +0300

    Bulid Entites

---
+++ Forums/main.py
@@ -0,0 +1,10 @@
+import models
+
+member1 = models.Member("Radi Obad ", 24)
+
+print member1
+
+
+post1 = models.Post("Post1", "Content1")
+
+print post1
--- Forums/models.py
+++ Forums/models.py
@@ -1,7 +1,21 @@
 class Member():
+	"""This class provides a way to store member name and age"""
+
 	def __init__(self, name, age):
+		self.id = 0
 		self.name = name
 		self.age = age
 
+	def __str__(self):
+		return '{} has {} years'.format(self.name, self.age)
 class Post():
-def __init__(self,)		
+	"""This class provides a way to store post title and content"""
+
+	def __init__(self, title, content):
+
+		self.id = 0
+		self.title = title
+		self.content= content
+
+	def __str__(self):
+		return	'{} : {}'.format(self.title, self.content)
